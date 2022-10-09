04d3a844265a4222df5f839d3db69b3b800e3c56
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2022-10-09 14:58:16 -0300

    agregado modal pedido

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let cliente = {
     pedido: []
 }
 
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', GuardarCliente);
 
@@ -33,7 +39,78 @@ function GuardarCliente(){
         return;
         
     } 
-        console.log(cliente);
+    // Asignar datos del formulario a cliente
+    cliente = { ...cliente, mesa, hora };
+
+    // Ocultar modal 
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+
+    // Mostrar las secciones
+    mostrarSecciones();
+
+    // Obtener platillos de la API de JSON-Server
+    obtenerPlatillos();
 
     
+}
+
+
+function mostrarSecciones(){
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+
+}
+
+function obtenerPlatillos(){
+    const url = 'http://localhost:4000/platillos';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarPlatillos(resultado))
+        .catch(error => console.log(error))
+        // .then()
+}
+
+function mostrarPlatillos(platillos){
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach( platillo => {
+        const row = document.createElement('div');
+        row.classList.add('row', 'py-3', 'border-top','fs-5');
+
+        const nombre = document.createElement('div');
+        nombre.classList.add('col-6','col-sm-6','col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        const precio = document.createElement('div');
+        precio.classList.add('col-6','col-sm-2','col-md-3', 'fw-bold');
+        precio.textContent = `$${platillo.precio}`;
+
+        const categoria = document.createElement('div');
+        categoria.classList.add('col-6','col-sm-2','col-md-3','fst-italic', 'fw-bold');
+        categoria.textContent = categorias[platillo.categoria];
+
+        const inputCantidad = document.createElement('input');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${platillo.id}`;
+        inputCantidad.classList.add('form-control');
+
+        //Funcion que detecta la cantidad y el platillo que se esta agregando
+
+        const agregar = document.createElement('div');
+        agregar.classList.add('col-6','col-sm-2','col-md-2', 'py-2');
+        agregar.appendChild(inputCantidad);
+
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+        row.appendChild(agregar);
+        contenido.appendChild(row);
+
+    })
 }
